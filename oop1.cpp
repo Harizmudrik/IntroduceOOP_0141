4a0ebd53c3a34c018f2b28f6f32c63eb3660b210
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-04-29 18:31:58 +0700

    membuat procedur printData

--- oop1.cpp
+++ oop1.cpp
@@ -6,7 +6,14 @@ class Mahasiswa{
     int nim;
     string nama;
     float nilai;
-    
+
+
+    void printData(){
+        cout << "Nim : " << nim << endl;
+        cout << "Nama :" << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+
+    }
 
 };
 
